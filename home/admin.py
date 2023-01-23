dee752d4752a3b9999df231112cbd168392a15a1
Cooper Hall <cooperhall111@gmail.com>
2023-01-23 17:29:29 -0500

    added intial seeds

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Example
+from .models import User
 
 # Register your models here.
-admin.site.register(Example)
+admin.site.register(User)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
-from .models import Example
+from .models import User
 
 # Create your views here.
 def index(request): #gets all data from Example
-	data = list(Example.objects.values())
+	data = list(User.objects.values())
 	return JsonResponse(data, safe=False)
